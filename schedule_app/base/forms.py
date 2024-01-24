340aa10aea6b34f0a9f861ab06deeff88b7d2377
Szymon Żaba <szymonzaba18@gmail.com>
2024-01-25 00:11:56 +0100

    add forms to project

--- schedule_app/base/forms.py
+++ schedule_app/base/forms.py
@@ -1,9 +1,17 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from base.models import Profile, User
+from base.models import Profile, User, Company
+
 
 class UserForm(UserCreationForm):
-    
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', ]
+        fields = ['username', 'first_name', 'last_name', 'inv_code', 'password1', 'password2']
+
+
+class CompanyForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = Company
+        fields = ['username', 'first_name', 'last_name', 'name', 'email', 'password1', 'password2']
