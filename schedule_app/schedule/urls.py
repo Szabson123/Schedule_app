ad0c43894804d8afdc8e3dad7b57603b477ec098
Szymon Żaba <szymonzaba18@gmail.com>
2024-01-30 22:56:14 +0100

    created calendar

--- schedule_app/schedule/urls.py
+++ schedule_app/schedule/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from schedule.views import main_page
+from schedule.views import main_page, month_calendar
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('main_page/', main_page, name='main_page')
+    path('main_page/', main_page, name='main_page'),
+    path('month_calendar/<int:year>/<str:month>/', month_calendar, name='month_calendar')
 ]
--- schedule_app/schedule/views.py
+++ schedule_app/schedule/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render, redirect
 import calendar
 from calendar import HTMLCalendar
 
+
 def main_page(request):
-    name = 'Szymon'
-    return render(request, 'schedule/main_page.html', {
-        'name': name
-        #'year': year,
-        #'month': month
+    return render(request, 'schedule/main_page.html')
+
+
+def month_calendar(request, year, month):
+    month = month.capitalize()
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+    cal = HTMLCalendar().formatmonth(year, month_number)
+
+    return render(request, 'schedule/month_calendar.html', {
+        'month': month,
+        'cal': cal
     })
