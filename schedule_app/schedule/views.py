5b5d8e458048e428e0e1773fa4dc9ab6b2ca44b2
Szabson123 <szymonzaba18@gmail.com>
2024-02-05 10:44:51 +0100

    added prev and next month buttons

--- schedule_app/schedule/views.py
+++ schedule_app/schedule/views.py
@@ -5,7 +5,7 @@ from calendar import HTMLCalendar
 from django.views.generic import ListView
 from datetime import datetime, date
 from django.utils.safestring import mark_safe
-
+from datetime import timedelta
 
 from base.models import Event
 from schedule.utils import UserCalendar
@@ -27,6 +27,19 @@ def month_calendar(request, year, month):
     })
 
 
+def prev_month(d):
+    first = d.replace(day=1)
+    prev_month = first - timedelta(days=1)
+    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    return month
+
+def next_month(d):
+    days_in_month = calendar.monthrange(d.year, d.month)[1]
+    last = d.replace(day=days_in_month)
+    next_month = last + timedelta(days=1)
+    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    return month
+
 class CalendarView(ListView):
     model = Event
     template_name = 'schedule/month_calendar.html'
@@ -35,11 +48,12 @@ class CalendarView(ListView):
         context = super().get_context_data(**kwargs)
         
         d = self.get_date(self.request.GET.get('day', None))
-        
         cal = UserCalendar(d.year, d.month)
         
         html_cal = cal.formatmonth(withyear=True)
         context['calendar'] = mark_safe(html_cal)
+        context['prev_month'] = prev_month(d)
+        context['next_month'] = next_month(d)
         return context
 
     @staticmethod
