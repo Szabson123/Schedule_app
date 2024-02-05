0876da9cfbcba219ee65558177e96554f5a3b974
Szabson123 <szymonzaba18@gmail.com>
2024-02-05 09:53:09 +0100

    add static files

--- schedule_app/schedule_app/settings.py
+++ schedule_app/schedule_app/settings.py
@@ -128,3 +128,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'schedule/main_page/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'schedule/static'),
+]
